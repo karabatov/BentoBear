a53b773fd2c2c0d7d54e026179e5bf0bdc062115
Yuri Karabatov <yuri@yurikarabatov.com>
2019-03-12 17:58:28 +0300

    Initial Commit

---
+++ BentoBear/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BentoBear
+//
+//  Created by Yuri Karabatov on 12/03/2019.
+//  Copyright © 2019 Yuri Karabatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
