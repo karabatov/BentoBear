e82c633edbf726fc4c0c9d65adc541842fac468f
Yuri Karabatov <yuri@yurikarabatov.com>
2019-03-13 19:44:49 +0300

    Add empty classes

---
+++ BentoBear/Note List/NoteListBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  NoteListBuilder.swift
+//  BentoBear
+//
+//  Created by Yuri Karabatov on 13/03/2019.
+//  Copyright © 2019 Yuri Karabatov. All rights reserved.
+//
+
+import Foundation
---
+++ BentoBear/Note List/NoteListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  NoteListViewModel.swift
+//  BentoBear
+//
+//  Created by Yuri Karabatov on 13/03/2019.
+//  Copyright © 2019 Yuri Karabatov. All rights reserved.
+//
+
+import Foundation
