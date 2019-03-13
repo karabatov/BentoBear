cdbc8c9e27944e1656ff6e4fd09e0803f704fa29
Yuri Karabatov <yuri@yurikarabatov.com>
2019-03-13 21:23:53 +0300

    Fill out simple Builder

--- BentoBear/Note List/NoteListBuilder.swift
+++ BentoBear/Note List/NoteListBuilder.swift
@@ -6,8 +6,37 @@
 //  Copyright © 2019 Yuri Karabatov. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactiveSwift
+import BentoKit
+import Result
 
 struct NoteListBuilder {
-    
+    func make(
+        context: NoteListRendererContext,
+        presentationFlow: Flow,
+        presenting: Flow
+    ) -> UIViewController {
+        let viewModel = NoteListViewModel()
+
+        let viewController = BoxViewController.init(
+            viewModel: viewModel,
+            renderer: NoteListRenderer.self,
+            rendererConfig: NoteListRenderer.Config(
+                context: context
+            ),
+            appearance: Property.init(value: NoteListRenderer.Appearance())
+        )
+
+        let flowController = NoteListFlowController(
+            presentationFlow: presentationFlow,
+            presenting: presenting
+        )
+
+        viewModel.routes
+            .observe(on: UIScheduler())
+            .observeValues(flowController.handle)
+
+        return viewController
+    }
 }
