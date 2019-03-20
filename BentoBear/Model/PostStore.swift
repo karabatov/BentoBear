a1590f58a4c7c341736a4a9f0d2935e837c0f493
Yuri Karabatov <yuri@yurikarabatov.com>
2019-03-20 17:10:39 +0300

    Saving with a signal producer

--- BentoBear/Model/PostStore.swift
+++ BentoBear/Model/PostStore.swift
@@ -9,20 +9,24 @@
 import Foundation
 import ReactiveSwift
 
+enum PostStoreError: Error {
+    case errorSaving
+}
+
 protocol PostStore {
     func loadAllOnDevice() -> [RichPost]
     /// Saves the posts on device.
     /// - Parameter overwrite: If `true`, overwrite posts with the same `PostID`.
     ///     When `false`, if the post already exists in the DB, it will not be overwritten.
-    func saveOnDevice(posts: [Post], overwrite: Bool)
+    func saveOnDevice(posts: [Post], overwrite: Bool) -> SignalProducer<Void, PostStoreError>
     /// Saves the users on device.
     /// - Parameter overwrite: If `true`, overwrite users with the same `UserID`.
     ///     When `false`, if the user already exists in the DB, it will not be overwritten.
-    func saveOnDevice(users: [User], overwrite: Bool)
+    func saveOnDevice(users: [User], overwrite: Bool) -> SignalProducer<Void, PostStoreError>
     /// Saves the comments on device.
     /// - Parameter overwrite: If `true`, overwrite comments with the same `CommentID`.
     ///     When `false`, if the comment already exists in the DB, it will not be overwritten.
-    func saveOnDevice(comments: [Comment], overwrite: Bool)
+    func saveOnDevice(comments: [Comment], overwrite: Bool) -> SignalProducer<Void, PostStoreError>
 }
 
 final class PostStoreDefaults: PostStore {
@@ -32,9 +36,21 @@ final class PostStoreDefaults: PostStore {
 
     private let defaults = UserDefaults.standard
     private let queue = DispatchQueue(label: "PostStoreDefaults")
-    private var postCache: Dictionary<PostID, Post>
-    private var userCache: Dictionary<UserID, User>
-    private var commentCache: Dictionary<PostID, [Comment]>
+    private var postCache: Dictionary<PostID, Post> {
+        didSet {
+            saveToDefaults(cache: postCache, key: PostStoreDefaults.postKey)
+        }
+    }
+    private var userCache: Dictionary<UserID, User>{
+        didSet {
+            saveToDefaults(cache: userCache, key: PostStoreDefaults.userKey)
+        }
+    }
+    private var commentCache: Dictionary<PostID, [Comment]> {
+        didSet {
+            saveToDefaults(cache: commentCache, key: PostStoreDefaults.commentKey)
+        }
+    }
 
     init() {
         if
@@ -53,7 +69,6 @@ final class PostStoreDefaults: PostStore {
             commentCache = [:]
             postCache = [:]
         }
-
     }
 
     func loadAllOnDevice() -> [RichPost] {
@@ -70,9 +85,13 @@ final class PostStoreDefaults: PostStore {
             })
     }
 
-    func saveOnDevice(posts: [Post], overwrite: Bool) {
-        queue.sync {
-            var newCache = postCache
+    func saveOnDevice(posts: [Post], overwrite: Bool) -> SignalProducer<Void, PostStoreError> {
+        return saveSync { [weak self] in
+            guard let strongSelf = self else {
+                throw PostStoreError.errorSaving
+            }
+
+            var newCache = strongSelf.postCache
             for post in posts {
                 guard newCache[post.id] == nil || overwrite else {
                     return
@@ -80,17 +99,17 @@ final class PostStoreDefaults: PostStore {
 
                 newCache[post.id] = post
             }
-            postCache = newCache
-
-            DispatchQueue.global().async { [weak self] in
-                self?.saveToDefaults(cache: newCache, key: PostStoreDefaults.postKey)
-            }
+            strongSelf.postCache = newCache
         }
     }
 
-    func saveOnDevice(users: [User], overwrite: Bool) {
-        queue.sync {
-            var newCache = userCache
+    func saveOnDevice(users: [User], overwrite: Bool) -> SignalProducer<Void, PostStoreError> {
+        return saveSync { [weak self] in
+            guard let strongSelf = self else {
+                throw PostStoreError.errorSaving
+            }
+
+            var newCache = strongSelf.userCache
             for user in users {
                 guard newCache[user.id] == nil || overwrite else {
                     return
@@ -98,17 +117,17 @@ final class PostStoreDefaults: PostStore {
 
                 newCache[user.id] = user
             }
-            userCache = newCache
-
-            DispatchQueue.global().async { [weak self] in
-                self?.saveToDefaults(cache: newCache, key: PostStoreDefaults.userKey)
-            }
+            strongSelf.userCache = newCache
         }
     }
 
-    func saveOnDevice(comments: [Comment], overwrite: Bool) {
-        queue.sync {
-            var newCache = commentCache
+    func saveOnDevice(comments: [Comment], overwrite: Bool) -> SignalProducer<Void, PostStoreError> {
+        return saveSync { [weak self] in
+            guard let strongSelf = self else {
+                throw PostStoreError.errorSaving
+            }
+
+            var newCache = strongSelf.commentCache
             for comment in comments {
                 var newComments = newCache[comment.postId] ?? []
                 if let idx = newComments.firstIndex(where: { $0.id == comment.id }), overwrite {
@@ -118,20 +137,33 @@ final class PostStoreDefaults: PostStore {
                 }
                 newCache[comment.postId] = newComments
             }
-            commentCache = newCache
+            strongSelf.commentCache = newCache
+        }
+    }
 
-            DispatchQueue.global().async { [weak self] in
-                self?.saveToDefaults(cache: newCache, key: PostStoreDefaults.commentKey)
+    private func saveSync(action: @escaping () throws -> Void) -> SignalProducer<Void, PostStoreError> {
+        return SignalProducer.init { [weak self] observer, _ in
+            self?.queue.sync {
+                defer { observer.sendCompleted() }
+
+                do {
+                    try action()
+                    observer.send(value: ())
+                } catch {
+                    observer.send(error: .errorSaving)
+                }
             }
         }
     }
 
     private func saveToDefaults<T: Encodable>(cache: T, key: String) {
-        guard let data = try? JSONEncoder().encode(cache) else {
-            return
-        }
+        DispatchQueue.global().async { [weak self] in
+            guard let data = try? JSONEncoder().encode(cache) else {
+                return
+            }
 
-        defaults.set(data, forKey: key)
-        defaults.synchronize()
+            self?.defaults.set(data, forKey: key)
+            self?.defaults.synchronize()
+        }
     }
 }
